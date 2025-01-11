6cd0c18d44b2163e6f73c0215d273d6b232144c9
FraProse23 <fra.prose23@gmail.com>
2025-01-11 07:19:18 +0000

    DD

---
+++ es3.cpp
@@ -0,0 +1,42 @@
+#include <iostream> #include <random> 
+
+using namespace std;
+int main()
+{ 
+    int m[4][4];
+    
+    int prodotto=1;
+    cout<<"inserisci una matrice 4x4 "<<endl;
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+                int >m[i][j]; = std::rand()%15;
+
+          
+           
+            
+              
+            
+        }}
+        for(int i=0;i<4;i++)
+     {
+        for(int j=0;j<4;j++)
+        {
+            if(i==j){
+                prodotto=m[i][j]*prodotto;
+
+            } else {
+             
+            }
+
+        
+        
+        }}
+        cout<<"IL PRODOTTO DEI VALORI DIAGONALI è PARI A" <<endl;
+         cout<<prodotto;
+         
+
+        
+        
+        }
